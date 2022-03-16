090c2171307a327b50f837915a3354e0d20b04c2
Ben Wilhelm <benjamin.m.wilhelm@gmail.com>
2022-03-16 16:52:31 -0500

    Cache Service happy path

--- services/fixtures/forecast-api.fixtures.js
+++ services/fixtures/forecast-api.fixtures.js
@@ -1,6 +1,7 @@
 export const mockTimeNow = 1644472858;
 export const mockTimeNowTopOfHour = 1644472800;
 export const mockTimeNowTopOfDay = 1644451200;
+export const mockZip = '60660';
 
 export const responseOneDayHourly = {
   location: {
