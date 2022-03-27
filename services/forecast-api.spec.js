8e46c24328e36e972d09b3b36db5d97f2d7e15d7
Ben Wilhelm <benjamin.m.wilhelm@gmail.com>
2022-03-27 10:13:24 -0500

    tests written for forecast api

--- services/forecast-api.spec.js
+++ services/forecast-api.spec.js
@@ -1,5 +1,22 @@
 import ForecastApiClient from './forecast-api.js';
+import { mockZip } from '../test-helpers/fixtures';
+import {
+  responseTenDayHourly,
+  mockApiKey,
+} from './fixtures/forecast-api.fixtures';
 
-test.todo('should fetch ten-day forecast by zip');
+test('should fetch ten-day forecast by zip', async () => {
+  const client = new ForecastApiClient({ apiKey: mockApiKey });
 
-test.todo('should throws 401 with bad API Key');
+  const response = await client.getForecastByZip(mockZip);
+
+  expect(response).toEqual(responseTenDayHourly);
+});
+
+test('should throws 401 with bad API Key', async () => {
+  const client = new ForecastApiClient({ apiKey: 'bad-api-key' });
+
+  const subject = () => client.getForecastByZip(mockZip);
+
+  expect(subject).rejects.toThrow(/401/g);
+});
